74e6b9f89ebacaa0ad6099bb3285f9b083c4e7c0
Natanael <natanael@timipro.com.br>
2020-10-27 20:51:44 -0300

    Criar uma Nova Thread sem travar a aplicação, utilizando uma classe que implementar Runnable

--- app/src/main/java/br/natanael/android/androidthreads/MainActivity.java
+++ app/src/main/java/br/natanael/android/androidthreads/MainActivity.java
@@ -15,9 +15,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void iniciarThread(View view){
-        MyThread thread = new MyThread();
-        thread.start();
+        /*MyThread thread = new MyThread();
+        thread.start();*/
+
+        MyRunnable runnable = new MyRunnable();
+        new Thread(runnable).start();
     }
+
+    class MyRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 15;i++){
+                Log.d("Thread", "contador: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     class MyThread extends  Thread {
         @Override
